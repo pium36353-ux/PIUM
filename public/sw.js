e38547a54c3629025c80bc7d30ffc2941369e458
Alessio <alessio.mei94@gmail.com>
2026-05-05 00:21:13 +0200

    PWA setup

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'pium-v1'
+
+const APP_SHELL = [
+  '/',
+  '/index.html',
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  // Solo richieste GET, escludi Supabase e API esterne
+  if (e.request.method !== 'GET') return
+  const url = new URL(e.request.url)
+  if (url.origin !== location.origin) return
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(cache => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
